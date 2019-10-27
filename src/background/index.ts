387e9fba6ab808b65e2e9719d3e7a0118949300a
Fabio Spampinato <spampinabio@gmail.com>
2019-10-27 02:56:21 +0100

    Hot reload: removed
    It wasn’t working anyway

--- src/background/index.ts
+++ src/background/index.ts
@@ -7,7 +7,6 @@ import '../../resources/icon/icon-48.png';
 import '../../resources/icon/icon-64.png';
 import '../../resources/icon/icon-128.png';
 import '../../resources/icon/icon-256.png';
-import './hot_reload';
 
 import Badge from './badge';
 import Config from './config';
